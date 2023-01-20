867a3e2ef6f7ac8a44b025f5fc3107021e3deeff
Worameth Semapat <worameth@ookbee.com>
2023-01-20 13:25:41 +0700

    feat: convert video to webm file

--- src/MyConsole/Program.cs
+++ src/MyConsole/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Reflection;
 using System.Threading.Tasks;
 using FFMpegCore;
+using FFMpegCore.Enums;
 
 namespace MyConsole
 {
@@ -11,7 +12,29 @@ namespace MyConsole
     {
         static string GetRootDirectory() => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-        static async Task Main(string[] args)
+        static async Task ConvertVideoToWebMFile()
+        {
+            var basePath = GetRootDirectory();
+            var inputFilePath = Path.Combine(basePath, "files", "sample_1920x1080.wmv");
+            var outputFilePath = Path.Combine(basePath, "files", "output.webm");
+
+            var success = await FFMpegArguments
+                .FromFileInput(inputFilePath)
+                .OutputToFile(outputFilePath, true, a => a
+                    .Seek(TimeSpan.FromSeconds(10))
+                    .WithDuration(TimeSpan.FromSeconds(30))
+                    .WithFramerate(30)
+                    .Resize(1920, 1080)
+                    .WithVideoCodec(VideoCodec.LibVpx)
+                    .WithConstantRateFactor(30)
+                    .WithVideoBitrate(4500)
+                    .DisableChannel(Channel.Audio)
+                ).ProcessAsynchronously();
+
+            Console.WriteLine($"Convert Success?: {success}");
+        }
+
+        static async Task GetThunbnailFileAsync()
         {
             var basePath = GetRootDirectory();
             var outputFile = Path.Combine(basePath, "thumbnail.png");
@@ -25,6 +48,13 @@ namespace MyConsole
                 )
                 .ProcessAsynchronously();
             Console.WriteLine($"Path: {outputFile}, Status: {success}");
+        }
+
+        static async Task Main(string[] args)
+        {
+            // await GetThunbnailFileAsync();
+
+            await ConvertVideoToWebMFile();
         }
     }
 }
